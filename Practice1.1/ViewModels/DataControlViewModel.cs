444c323a5ae50a70b14412efc818cb842d1a1c2a
Ілля Мельник <illia.melnyk@ukma.edu.ua>
2022-02-13 22:21:28 +0200

    Added some business logic

--- Practice1.1/ViewModels/DataControlViewModel.cs
+++ Practice1.1/ViewModels/DataControlViewModel.cs
@@ -7,7 +7,7 @@ using System.ComponentModel;
 using Practice1._1.Models;
 using Practice1._1.Tools;
 using System.Runtime.CompilerServices;
-
+using System.Windows;
 
 
 namespace Practice1._1.ViewModels
@@ -21,31 +21,35 @@ namespace Practice1._1.ViewModels
         private string _age;
         private string _chineseData;
         private string _westData;
+        private enum WestDataEnum { Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces };
+        private enum ChineseDataEnum { Monkey, Rooster, Dog, Pig, Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat };
         #endregion
 
         #region Properties
         public DateTime BDate
         {
-            get{ return _date.BDate; }
-            set{ _date.BDate = value; }
+            get { if (_date.BDate == DateTime.MinValue)
+                    return DateTime.Now;
+                return _date.BDate; }
+            set { _date.BDate = value; OnChanged(); }
         }
 
         public string Age
         {
-            get { return _age;}
-            set{ _age = value; OnChanged();}
+            get { return _age; }
+            set { _age = value; OnChanged(); }
         }
 
         public string WestData
         {
-            get {return _westData; }
-            set { _westData = value; OnChanged();}
+            get { return _westData; }
+            set { _westData = value; OnChanged(); }
         }
 
         public string ChineseData
         {
-            get {return _chineseData; }
-            set { _chineseData = value; OnChanged();}
+            get { return _chineseData; }
+            set { _chineseData = value; OnChanged(); }
         }
 
         public RelayCommand<object> CheckDataCommand
@@ -56,18 +60,60 @@ namespace Practice1._1.ViewModels
             }
         }
 
+
+        #endregion
+
+        #region BusinessLogic
         private bool CanExecute(object obj)
         {
             return true;
         }
-
         private void Check()
         {
-            Age = "ok";
-            WestData = "ok";
-            ChineseData = "ok";
-            Console.WriteLine("ok");
+            if (isValidBDate())
+            {
+                if(_date.СurrentDate.Month == BDate.Month && _date.СurrentDate.Day == BDate.Day)
+                    MessageBox.Show("Happy B-Day!!!");
+                Age = ageValue().ToString();
+                WestData = WestDataSign();
+                ChineseData = ChineseDataSign();
+                return;
+            }
+            clearFields();                   
         }
+
+        private void clearFields()
+        {
+            Age = "";
+            WestData = "";
+            ChineseData = "";
+            BDate = DateTime.Now;
+            MessageBox.Show("Illegal Data: write real BDay and your age can't be higher than 135 ");
+        }
+    
+        private bool isValidBDate()
+        {
+            if(ageValue() > 135 || BDate.CompareTo(_date.СurrentDate) > 0)
+                return false;
+            return true;
+        }
+        private string WestDataSign()
+        {
+            return (ChineseDataEnum)(BDate.Year % 12) + "";
+        }
+
+        private string ChineseDataSign()
+        {
+            return (ChineseDataEnum)((BDate.Year % 12)) + "";
+        }
+
+        private int ageValue()
+        {
+            if (BDate.Month.CompareTo(_date.СurrentDate.Month) <= 0 && BDate.Day.CompareTo(_date.СurrentDate.Day) <= 0)
+                return _date.СurrentDate.Year - BDate.Year;
+            return _date.СurrentDate.Year - BDate.Year - 1;
+        }
+
         #endregion
 
         protected virtual void OnChanged([CallerMemberName] string propertyName = null)
