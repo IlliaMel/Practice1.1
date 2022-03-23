15e7108f6be7e0ece66e8853369c388ff6c32a7e
Ілля Мельник <illia.melnyk@ukma.edu.ua>
2022-03-23 12:32:17 +0200

    Practice2

---
+++ Practice1.1/Models/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1._1.Models
+{
+    class Person
+    {
+
+        #region Fileds
+        private String _name;
+        private String _surname;
+        private String _email;
+        private DateTime _bDate;
+        #endregion
+
+
+        #region Constructors
+        public Person(String name, String surname, String email, DateTime bDate)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            BDate = bDate;
+        }
+
+        public Person(String name, String surname, String email)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+        }
+
+        public Person(String name, String surname, DateTime bDate)
+        {
+            Name = name;
+            Surname = surname;
+            BDate = bDate;
+        }
+        #endregion
+
+        #region Properties
+        public String Name
+        {
+            get { return _name; }
+            private set { _name = value; }
+        }
+        public String Surname
+        {
+            get { return _surname; }
+            private set { _surname = value; }
+        }
+        public String Email
+        {
+            get { return _email; }
+            private set { _email = value; }
+        }
+        public DateTime BDate
+        {
+            get { return _bDate; }
+            private set { _bDate = value; }
+        }
+        #endregion
+
+    }
+}
